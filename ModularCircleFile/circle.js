0d0a1842859074062d9bc6b3e5c248f1ba0982dc
eneskocadag <eneskocadag5e@gmail.com>
2021-12-31 18:36:35 +0300

    CircleArea-Circumference

---
+++ ModularCircleFile/circle.js
@@ -0,0 +1,11 @@
+function circleArea(r) {
+  console.log(Math.pow(r, 2) * Math.PI);
+}
+function circleCircumference(r) {
+  console.log(2 * Math.PI * r);
+}
+
+module.exports = {
+  circleArea,
+  circleCircumference,
+};
